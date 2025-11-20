f0a6e649f37961f5f74ede6e80b788e9415ccf43
Agustin <agustinpaternoste@gmail.com>
2025-11-20 13:56:52 +0100

    fixing errors on db_user_pass

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -12,11 +12,12 @@ done
 
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_pass)
 DB_ADMIN_PASSWORD=$(cat /run/secrets/db_admin_pass)
+password=$(cat /run/secrets/db_user_pass)
 
 mysql -u root << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$password';
 CREATE USER IF NOT EXISTS '$DB_ADMIN'@'%' IDENTIFIED BY '$DB_ADMIN_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO  '$DB_ADMIN'@'%';
 GRANT ALL PRIVILEGES ON  \`$DB_NAME\`.* TO '$DB_USER'@'%';
